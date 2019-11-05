6f36ee551cb490b90afcaa3582347e798ad1a584
HaniyaAnis <55890173+HaniyaAnis@users.noreply.github.com>
2019-11-05 22:06:04 +0500

    Add files via upload

---
+++ Arranging array in ascending order.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim marks(8) As Integer
+        Dim StuMarks, index, count, a, b, C, D, E As Integer
+
+
+        stumarks = 0
+        index = 0
+        a = 0
+        count = 0
+        B = 0
+        C = 0
+        D = 0
+        E = 0
+
+        For COUNT = 1 To 8
+            marks(count) = 0
+        Next
+
+        For Index = 1 To 8
+            Console.Write(" ENTER MARKS OF STUDENT NO " & index & ": ")
+            StuMarks = Console.ReadLine
+            marks(index) = StuMarks
+        Next
+
+
+        For a = 7 To 1 Step -1
+            For C = 1 To a
+                If marks(C) > marks(C + 1) Then
+                    b = marks(C)
+                    marks(C) = marks(C + 1)
+                    marks(C + 1) = b
+                End If
+            Next
+        Next
+
+        Console.WriteLine("MARKS IN ASCENDING ORDER ARE: ")
+        For D = 1 To 8
+            E = marks(D)
+            Console.WriteLine(E)
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ searching array.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Marks(8) As Integer
+        Dim StuMarks, INDEX, COUNT, A, COUNTER As Integer
+        Dim Found As Boolean
+
+        stumarks = 0
+        index = 0
+        a = 0
+        count = 0
+        Found = False
+        Counter = 0
+
+        For COUNT = 1 To 8
+            Marks(COUNT) = 0
+        Next COUNT
+
+        For Index = 1 To 8
+            Console.Write(" ENTER MARKS OF STUDENT NO " & INDEX & ": ")
+            StuMarks = Console.ReadLine
+            Marks(INDEX) = StuMarks
+        Next Index
+
+
+        Console.Write("enter marks to be searched in array: ")
+        A = Console.ReadLine
+        For COUNTER = 1 To 8
+            If Marks(COUNTER) = A Then
+                Found = True
+                Exit For
+            End If
+        Next
+
+        If Found = True Then
+            Console.Write(A & " " & "is found in array at location " & COUNTER)
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
